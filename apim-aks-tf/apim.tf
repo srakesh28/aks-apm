382da9f5c158b1225dbb19eaf905291c6070dd28
srakesh28 <srakesh28@yahoo.com>
2019-10-15 22:49:54 -0700

    update apmi

--- apim-aks-tf/apim.tf
+++ apim-aks-tf/apim.tf
@@ -1,9 +1,9 @@
 resource "azurerm_api_management" "apim" {
-  name                = "blog-apim"
+  name                = "aks-apim"
   location            = "${azurerm_resource_group.apim-aks.location}"
   resource_group_name = "${azurerm_resource_group.apim-aks.name}"
-  publisher_name      = "Nills"
-  publisher_email     = "nilfran@microsoft.com"
+  publisher_name      = "Rakesh Shah"
+  publisher_email     = "raksha@microsoft.com"
 
   sku {
     name     = "Developer"
